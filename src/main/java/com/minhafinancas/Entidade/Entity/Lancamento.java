5e1b67c9423d2bf8a20a55ec71b36fb1fa901ccf
Murilo Henrique <34652210+murilothink@users.noreply.github.com>
2020-07-28 09:06:48 -0300

    Refatoração de codigo, usando lombok

--- src/main/java/com/minhafinancas/Entidade/Entity/Lancamento.java
+++ src/main/java/com/minhafinancas/Entidade/Entity/Lancamento.java
@@ -3,6 +3,8 @@ package com.minhafinancas.Entidade.Entity;
 
 import com.minhafinancas.Entidade.Enum.StatusLancamento;
 import com.minhafinancas.Entidade.Enum.TipoLancamento;
+import lombok.Builder;
+import lombok.Data;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
@@ -10,6 +12,8 @@ import java.util.Objects;
 
 @Entity
 @Table(name = "Lancamento", schema = "financas")
+@Data // Lombok, gera os getters e setters, toString entre outros.
+@Builder
 public class Lancamento {
 
     @Id
@@ -46,102 +50,4 @@ public class Lancamento {
     @Enumerated(value = EnumType.STRING)
     private StatusLancamento status;
 
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public Integer getAno() {
-        return ano;
-    }
-
-    public void setAno(Integer ano) {
-        this.ano = ano;
-    }
-
-    public Integer getMes() {
-        return mes;
-    }
-
-    public void setMes(Integer mes) {
-        this.mes = mes;
-    }
-
-    public Usuario getUsuario() {
-        return usuario;
-    }
-
-    public void setUsuario(Usuario usuario) {
-        this.usuario = usuario;
-    }
-
-    public BigDecimal getValor() {
-        return valor;
-    }
-
-    public void setValor(BigDecimal valor) {
-        this.valor = valor;
-    }
-
-    public TipoLancamento getTipo() {
-        return tipo;
-    }
-
-    public void setTipo(TipoLancamento tipo) {
-        this.tipo = tipo;
-    }
-
-    public StatusLancamento getStatus() {
-        return status;
-    }
-
-    public void setStatus(StatusLancamento status) {
-        this.status = status;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Lancamento that = (Lancamento) o;
-        return id.equals(that.id) &&
-                descricao.equals(that.descricao) &&
-                ano.equals(that.ano) &&
-                mes.equals(that.mes) &&
-                usuario.equals(that.usuario) &&
-                valor.equals(that.valor) &&
-                tipo == that.tipo &&
-                status == that.status;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, descricao, ano, mes, usuario, valor, tipo, status);
-    }
-
-    @Override
-    public String toString() {
-        return "Lancamento{" +
-                "id=" + id +
-                ", descricao='" + descricao + '\'' +
-                ", ano=" + ano +
-                ", mes=" + mes +
-                ", usuario=" + usuario +
-                ", valor=" + valor +
-                ", tipo=" + tipo +
-                ", status=" + status +
-                '}';
-    }
 }
--- src/main/java/com/minhafinancas/Entidade/Entity/Usuario.java
+++ src/main/java/com/minhafinancas/Entidade/Entity/Usuario.java
@@ -1,10 +1,14 @@
 package com.minhafinancas.Entidade.Entity;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.Objects;
 
 @Entity
 @Table( name = "usuario", schema = "financas")
+@Data
+@Builder
 public class Usuario {
 
     @Id
@@ -19,63 +23,7 @@ public class Usuario {
     private String nome;
 
     @Column(name = "senha")
+
     private String senha;
 
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Usuario usuario = (Usuario) o;
-        return id == usuario.id &&
-                email.equals(usuario.email) &&
-                nome.equals(usuario.nome) &&
-                senha.equals(usuario.senha);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, email, nome, senha);
-    }
-
-    @Override
-    public String toString() {
-        return "Usuario{" +
-                "id=" + id +
-                ", email='" + email + '\'' +
-                ", nome='" + nome + '\'' +
-                ", senha='" + senha + '\'' +
-                '}';
-    }
 }
